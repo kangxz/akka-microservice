527c57fa97cc7f01fee90dcda1c47a39897f9723
Bartłomiej Szwej <bartszw537@student.polsl.pl>
2016-11-11 14:52:21 +0100

    Step 2: HashtagManagerActor and endpoint integration

--- src/main/scala/done/io/github/bszwej/api/HashtagEndpoint.scala
+++ src/main/scala/done/io/github/bszwej/api/HashtagEndpoint.scala
@@ -1,20 +1,8 @@
-/*
- * [y] hybris Platform
- *
- * Copyright (c) 2000-2016 hybris AG
- * All rights reserved.
- *
- * This software is the confidential and proprietary information of hybris
- * ("Confidential Information"). You shall not disclose such Confidential
- * Information and shall use it only in accordance with the terms of the
- * license agreement you entered into with hybris.
- */
 package done.io.github.bszwej.api
 
 import akka.actor.ActorRef
-import akka.http.scaladsl.model.StatusCodes
 import akka.pattern.ask
-import done.io.github.bszwej.HashtagManagerActor.AddHashtag
+import done.io.github.bszwej.HashtagManagerActor.{AddHashtag, HashtagAdded}
 
 import scala.util.{Failure, Success}
 
@@ -23,12 +11,12 @@ class HashtagEndpoint(hashtagManagerActor: ActorRef) extends BaseEndpoint {
   val route = path("hashtags" / Segment) { hashtag ⇒
     pathEnd {
       post {
-        onComplete((hashtagManagerActor ? AddHashtag(hashtag))) {
-          case Success(_) ⇒
-            complete("Hashtag added!")
+        onComplete((hashtagManagerActor ? AddHashtag(hashtag)).mapTo[HashtagAdded]) {
+          case Success(ha) ⇒
+            complete(s"Hashtag '${ha.tagName}' added!")
           case Failure(e) ⇒
             logger.error(s"Error occurred: ${e.getMessage}")
-            complete((StatusCodes.InternalServerError, "Error!"))
+            complete(s"Failure when adding hashtag: ${e.getMessage}")
         }
       }
     }
